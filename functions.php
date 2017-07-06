72a0f3cc25484f9c9f601b1c43a6823d4d3485f6
jorge <jorge.f@velosoft.mx>
2017-07-06 18:54:27 -0500

    PRoveedores

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+  function get_header(){
+    include("head.php");
+  }
+  function get_headerPrincipal(){
+    include("headPrincipal.php");
+  }
+
+  function get_footer(){
+    include("footer.php");
+  }
+  function get_aside(){
+  	include("aside.php");
+  }
+  function get_footer_graficas(){
+  	include("footer-graficas.php");
+  }
+?>
